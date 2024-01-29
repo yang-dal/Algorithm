5c10af3f1f010d254459b68a2f82408700829e5c
yang-dal <157715004+yang-dal@users.noreply.github.com>
2024-01-29 17:43:20 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        double C = (double) A / B;
+        System.out.println(C);
+    }
+}
