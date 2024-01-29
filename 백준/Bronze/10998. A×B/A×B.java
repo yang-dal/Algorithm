f0557e373919f9dd33090fed1d57fbd4639083ae
yang-dal <157715004+yang-dal@users.noreply.github.com>
2024-01-29 17:39:53 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int C = A * B;
+        System.out.println(C);
+    }
+}
