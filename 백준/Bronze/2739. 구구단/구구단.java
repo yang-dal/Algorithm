311152b4b2b3e4c0aefd51f7522d87a944336814
yang-dal <157715004+yang-dal@users.noreply.github.com>
2024-01-30 17:43:41 +0900

    [Bronze V] Title: 구구단, Time: 244 ms, Memory: 18660 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(N + " * " + i + " = " + N * i);
+        }
+    }
+}
